6da9211b9115ef2cedde16f1f477146d5da1ff67
Jacob Hubbard <hubbarj6@miamioh.edu>
2021-04-13 19:18:59 -0400

    Create block code

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+//func CreateBlock(data string, prevHash []byte) *Block {
+
+//}
+
+func main() {
+	fmt.Println([]byte("This is a test"))
+}
