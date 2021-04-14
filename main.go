ecc2fd8e8de181e36752e3b94d2a3a4bb31420de
Jacob Hubbard <hubbarj6@miamioh.edu>
2021-04-14 18:21:51 -0400

    Proof of work complete

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math/big"
+	"strconv"
+
+	"github.com/HubbardJacob/Vehicle-Exchange-Blockchain/blockchain"
 )
 
 func main() {
-	// chain := blockchain.InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
-	// chain.AddBlock("First Block")
-	// chain.AddBlock("Second Block")
-	// chain.AddBlock("Third Block")
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
 
-	// for _, block := range chain.blocks {
-	// 	fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-	// 	fmt.Printf("Data in Block: %s\n", block.Data)
-	// 	fmt.Printf("Hash: %x\n", block.Hash)
-	// }
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
 
-	target := big.NewInt(1)
-	fmt.Println(target.Lsh(target, uint(256-250)))
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
 }
